860027ed22d79f877bacf8131616ab0897f2898a
Urška <137105614+gitnubnub@users.noreply.github.com>
2023-08-10 14:18:00 +0200

    Update naloga2 (dn10 - preimenuj!).c
    optimizacija

--- naloga2 (dn10 - preimenuj!).c
+++ naloga2 (dn10 - preimenuj!).c
@@ -8,7 +8,7 @@
 
 #include "naloga2.h"
 
-Ulomek uredi(Ulomek u) {
+/*Ulomek uredi(Ulomek u) {
     if (u.im < 0) {
         u.st *= (-1);
         u.im *= (-1);
@@ -89,6 +89,82 @@ Tocka projekcija(Tocka t, Premica p) {
     }
 
     return *presecisce;
+}*/
+
+//optimizirana rešitev (delajo vsi testni primeri):
+
+Ulomek sestej(Ulomek prvi, Ulomek drugi) {
+    Ulomek* rezultat = (Ulomek*) calloc(1, sizeof(Ulomek));
+
+    if (prvi.im == drugi.im) {
+        rezultat -> im = prvi.im;
+        rezultat -> st = prvi.st + drugi.st;
+    } else {
+        rezultat -> im = prvi.im * drugi.im;
+        rezultat -> st = prvi.st * drugi.im + drugi.st * prvi.im;
+    }
+
+    return *rezultat;
+}
+
+Ulomek odstej(Ulomek prvi, Ulomek drugi) {
+    drugi.st = - drugi.st;
+    return sestej(prvi, drugi);
+}
+
+Ulomek zmnozi(Ulomek prvi, Ulomek drugi) {
+    Ulomek* rezultat = (Ulomek*) calloc(1, sizeof(Ulomek));
+
+    rezultat -> st = prvi.st * drugi.st;
+    rezultat -> im = prvi.im * drugi.im;
+
+    return *rezultat;
+}
+
+Ulomek deli(Ulomek prvi, Ulomek drugi) {
+    int temp = drugi.st;
+    drugi.st = drugi.im;
+    drugi.im = temp;
+    return zmnozi(prvi, drugi);
+}
+
+Ulomek okrajsaj(Ulomek ulomek) {
+    if (ulomek.im < 0) {
+        ulomek.st = - ulomek.st;
+        ulomek.im = - ulomek.im;
+    }
+
+    for (int i = (abs(ulomek.st) > abs(ulomek.im) ? abs(ulomek.im) : abs(ulomek.st)); i > 1; i--) {
+        if (ulomek.st % i == 0 && ulomek.im % i == 0) {
+            ulomek.st /= i;
+            ulomek.im /= i;
+            i = (abs(ulomek.st) > abs(ulomek.im) ? abs(ulomek.im) : abs(ulomek.st)) + 1;
+        }
+    }
+
+    return ulomek;
+}
+
+Tocka projekcija(Tocka t, Premica p) {
+    Tocka* A = (Tocka*) calloc(1, sizeof(Tocka));
+
+    if (p.navpicna) {
+        A -> x = p.n;
+        A -> y = t.y;
+    } else if (p.k.st == 0) {
+        A -> x = t.x;
+        A -> y = p.n;
+    } else {
+        Ulomek nPrav = sestej(t.y, deli(t.x, p.k));
+        Ulomek* ena = (Ulomek*) calloc(1, sizeof(Ulomek));
+        ena -> st = 1;
+        ena -> im = 1;
+
+        A -> x = okrajsaj(deli(odstej(nPrav, p.n), sestej(p.k, deli(*ena, p.k))));
+        A -> y = okrajsaj(sestej(zmnozi(p.k, A -> x), p.n));
+    }
+
+    return *A;
 }
 
 #ifndef test
