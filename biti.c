38eec2a745cc977ffaf88d64a265ac24e0a61e3b
Urška <137105614+gitnubnub@users.noreply.github.com>
2023-08-12 18:30:50 +0200

    Update biti.c
    dodana razumljivejša verzija

--- biti.c
+++ biti.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -34,6 +34,42 @@ int main(int argc, char* argv[]) {
     printf("\n");
     free(tabelaBitov);  
     free(preberiNajprej);
+    fclose(input);
+    return 0;
+}*/
+
+//meni bolj razumljiva rešitev:
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    FILE* input = fopen(argv[1], "rb");
+    int p = atoi(argv[2]);
+    int q = atoi(argv[3]);
+
+    unsigned char* nezanimivi = (unsigned char*) calloc(p / 8, sizeof(unsigned char));
+    int prebrano = fread(nezanimivi, sizeof(unsigned char), p / 8, input);
+    free(nezanimivi);
+    int* biti = (int*) calloc(q + 8, sizeof(int));
+
+    int index = prebrano * 8;
+    unsigned char byte = 0;
+    while (index < q) {
+        fread(&byte, sizeof(unsigned char), 1, input);
+
+        for (int i = 7; i >= 0; i--) {
+            biti[index] = (byte >> i) & 1;
+            index++;
+        }
+    }
+
+    for (int i = p; i < q; i++) {
+        printf("%d", biti[i]);
+    }
+    printf("\n");
+
+    free(biti);
     fclose(input);
     return 0;
 }
